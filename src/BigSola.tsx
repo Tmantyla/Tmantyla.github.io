2b49ee2613fa12c06a94970c153002fd9acff46c
Luukas Lehtinen <luukas.lehtinen@paivola.fi>
2022-07-04 18:47:36 +0300

    Display solakuva

---
+++ src/BigSola.tsx
@@ -0,0 +1,11 @@
+import Sola from './assets/images/click.png'
+
+const BigSola = () => {
+    return(
+        <div className="BigSola">
+          <img  src={Sola} alt="fireSpot"/>
+        </div>
+    )
+}
+
+export default BigSola;
